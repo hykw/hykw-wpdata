9783cf4280f9b716acea9c4a358611e0558281b7
hykw <hykw1234@gmail.com>
2014-06-21 19:29:27 +0900

    added some functions

--- main.php
+++ main.php
@@ -29,15 +29,147 @@ class hykwWPData
         
   
   ### 投稿関係
+  # 投稿のIDを取得
+  public static function get_in_post_id()
+  {
+    return get_the_ID();
+  }
+
+  # 投稿のパーマリンクを取得(ループ内で使う場合は $postid を指定しない）
+  public static function get_post_permalink($postid = '')
+  {
+    $url = null;
+    if ($postid == '')
+      $url = get_permalink();
+
+    $url =  get_permalink($postid);
+    if ($url == FALSE)
+      $url = '';
+
+    return $url;
+  }
+  public static function get_in_post_permalink()
+  {
+    return hykwWPData::get_post_permalink();
+  }
+
+
   # タイトルの取得
   public static function get_post_title($sep = '')
   {
     if ($sep != '')
-      return wp_title($sep, false, 'right');
+      return wp_title($sep, FALSE, 'right');
     else
-      return wp_title($sep, false);
+      return wp_title($sep, FALSE);
+  }
+  public static function get_in_post_title()
+  {
+    return get_the_title();
+  }
+  
+
+  # サムネイルのURLを取得
+  public static function get_post_thumbnail($postid, $size = 'full')
+  {
+    $thum_id = get_post_thumbnail_id($postid);
+    if (is_null($thum_id))
+      return '';
+    
+    $image = wp_get_attachment_image_src($thum_id, $size);
+
+    return $image[0];
+  }
+
+  # 本文を取得
+  public static function get_post_content($postid)
+  {
+    $post = get_post($postid);
+    return $post->post_content;
+  }
+  public static function get_in_post_content()
+  {
+    return get_the_content();
+  }
+
+
+
+
+  # 抜粋を取得
+  public static function get_post_excerpt($postid)
+  {
+    $post = get_post($postid);
+    if ($post->excerpt != '')
+      return $post->excerpt;
+
+    $content = strip_shortcodes($post->post_content);
+    $content = apply_filters('the_content', $content);
+    $excerpt = wp_trim_words($content);
+
+    return $excerpt;
+  }
+  public static function get_in_post_excerpt()
+  {
+    return get_the_excerpt();
+  }
+
+  # 投稿日の取得
+  public static function get_post_time($postid)
+  {
+    return get_post_time(get_option('date_format'), FALSE, $postid);
+  }
+  public static function get_in_post_time()
+  {
+    return get_the_time(get_option('date_format'));
+  }
+  
+
+
+
+  /*
+# http://wpdocs.sourceforge.jp/%E3%83%86%E3%83%B3%E3%83%97%E3%83%AC%E3%83%BC%E3%83%88%E3%82%BF%E3%82%B0/get_the_category
+  # カテゴリオブジェクトの配列を返す
+  public static function get_post_categories($postid = null)
+  {
+    if (is_null($postid))
+      return get_the_category();
+    else
+      return get_the_category($postid);
+  }
+  */
+
+  # 投稿のカテゴリ名の取得(１つしか設定されてない場合、文字列で返す。複数の場合は配列)
+  public static function get_post_categoryNames($postid = FALSE)
+  {
+    if ($postid == FALSE)
+      $cats = get_the_category();
+    else
+      $cats = get_the_category($postid);
+
+    switch (count($cats)) {
+        case 0: 
+          return '';
+        case 1:
+          return $cats[0]->name;
+    }
+    
+    $ret = array();
+    foreach ($cats as $cat) {
+      array_push($ret, $cat->name);
+    }
+    return $ret;
+  }
+
+
+  ### カテゴリ関係
+  # 選択されたカテゴリ名を返す（選択されてない場合はFALSE)
+  public static function get_category_name()
+  {
+    $catid = get_query_var('cat');
+    if ($catid != '')
+      return get_cat_name($catid);
+
+    return FALSE;
   }
   
   
-
 }
