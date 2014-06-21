5adc316358ea3b4f608b975911867d25799474ce
hykw <hykw1234@gmail.com>
2014-06-21 18:02:39 +0900

    make functions static

--- main.php
+++ main.php
@@ -13,18 +13,31 @@ Version: 0.1
 
 class hykwWPData
 {
-	### ブログ関係
-	# ブログの名称を取得
-  public function get_blog_name()
-	{
-		return get_bloginfo('name');
-	}
+  ### ブログ関係
 
-	# ブログの説明を取得
-	public function get_blog_desc()
-	{
-		return get_bloginfo('description');
-	}
-	
+  # ブログの名称を取得
+  public static function get_blog_name()
+  {
+    return get_bloginfo('name');
+  }
+
+  # ブログの説明を取得
+  public static function get_blog_description()
+  {
+    return get_bloginfo('description');
+  }
+        
+  
+  ### 投稿関係
+  # タイトルの取得
+  public static function get_post_title($sep = '')
+  {
+    if ($sep != '')
+      return wp_title($sep, false, 'right');
+    else
+      return wp_title($sep, false);
+  }
+  
+  
+
 }
-
