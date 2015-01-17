befdc6813397d1029cb532519c05e7ca2c3e8001
hykw <hykw1234@gmail.com>
2015-01-17 15:34:58 +0900

    Revert "backup"
    This reverts commit 3e175147522522b109f445be0f069aad76c375c2.

---
+++ tests/base.php
@@ -0,0 +1,19 @@
+<?php
+
+class xxx extends WP_UnitTestCase {
+  public function setUp()
+  {
+    parent::setUp();
+  }
+
+  public function tearDown()
+  {
+    parent::tearDown();
+  }
+
+  public function test_()
+  {
+
+  }
+
+}
