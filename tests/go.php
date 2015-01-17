e8d63ffbcfaf943cb6b3973c964f30a8c9e70858
hykw <hykw1234@gmail.com>
2015-01-17 14:49:06 +0900

    backup

---
+++ tests/go.php
@@ -0,0 +1,11 @@
+<?php
+
+class sample extends WP_UnitTestCase {
+
+    public function test_rewrite()
+    {   
+#        $this->go_to( '/hoge/' );
+        $this->go_to( '/' );
+        $this->assertQueryTrue( 'is_home' ); // is_singular()なら合格
+    }   
+}
