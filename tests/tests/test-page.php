f32c914a5a0adcf14c316c29aa929a97c1042836
hykw <hykw1234@gmail.com>
2015-01-18 16:15:44 +0900

    fix

--- tests/tests/test-page.php
+++ tests/tests/test-page.php
@@ -16,49 +16,49 @@ class UT_hykwWPData_page extends hykwEasyUT {
     $this->assertEquals($pageid, hykwWPData_page::iget_id());
 
     ##### *get_parent_id()
-  $this->assertEquals(2119, hykwWPData_page::iget_parent_id());
-  $this->assertEquals(2119, hykwWPData_page::get_parent_id($pageid));
-  $this->assertEquals(FALSE, hykwWPData_page::get_parent_id(1751));
-  $this->assertEquals(FALSE, hykwWPData_page::get_parent_id(99999));
+    $this->assertEquals(2119, hykwWPData_page::iget_parent_id());
+    $this->assertEquals(2119, hykwWPData_page::get_parent_id($pageid));
+    $this->assertEquals(FALSE, hykwWPData_page::get_parent_id(1751));
+    $this->assertEquals(FALSE, hykwWPData_page::get_parent_id(99999));
 
-  ##### *get_objects()
-  $expects = array(
-    'ID'=> $pageid,
-    'post_name' => 'static1',
-  );
-  $this->assertEquals($expects, hykwWPData_page::iget_objects(array('ID', 'post_name')));
-  $this->assertEquals($expects, hykwWPData_page::get_objects('/parent/static1', FALSE, array('ID', 'post_name')));
-  $this->assertEquals($expects, hykwWPData_page::get_objects(FALSE, $pageid, array('ID', 'post_name')));
-  $this->assertEquals(FALSE, hykwWPData_page::get_objects(FALSE, FALSE));
-  $this->assertEquals(FALSE, hykwWPData_page::get_objects('/xxxxxxxxx', FALSE));
+    ##### *get_objects()
+    $expects = array(
+      'ID'=> $pageid,
+      'post_name' => 'static1',
+    );
+    $this->assertEquals($expects, hykwWPData_page::iget_objects(array('ID', 'post_name')));
+    $this->assertEquals($expects, hykwWPData_page::get_objects('/parent/static1', FALSE, array('ID', 'post_name')));
+    $this->assertEquals($expects, hykwWPData_page::get_objects(FALSE, $pageid, array('ID', 'post_name')));
+    $this->assertEquals(FALSE, hykwWPData_page::get_objects(FALSE, FALSE));
+    $this->assertEquals(FALSE, hykwWPData_page::get_objects('/xxxxxxxxx', FALSE));
 
-  ##### *get_object()
-  $this->assertEquals($pageid, hykwWPData_page::iget_object('ID'));
-  $this->assertEquals($pageid, hykwWPData_page::get_object('ID', FALSE, $pageid));
+    ##### *get_object()
+    $this->assertEquals($pageid, hykwWPData_page::iget_object('ID'));
+    $this->assertEquals($pageid, hykwWPData_page::get_object('ID', FALSE, $pageid));
 
-  ##### *get_permalink()
-  $this->assertEquals($fqdnAndPath, hykwWPData_page::iget_permalink(FALSE));
-  $this->assertEquals('/parent/static1', hykwWPData_page::iget_permalink(TRUE));
-  $this->assertEquals('/parent/static1', hykwWPData_page::iget_permalink());
+    ##### *get_permalink()
+    $this->assertEquals($fqdnAndPath, hykwWPData_page::iget_permalink(FALSE));
+    $this->assertEquals('/parent/static1', hykwWPData_page::iget_permalink(TRUE));
+    $this->assertEquals('/parent/static1', hykwWPData_page::iget_permalink());
 
-  $this->assertEquals($fqdnAndPath, hykwWPData_page::get_permalink($pageid, FALSE));
-  $this->assertEquals('/parent/static1', hykwWPData_page::get_permalink($pageid, TRUE));
-  $this->assertEquals('/parent/static1', hykwWPData_page::get_permalink($pageid));
+    $this->assertEquals($fqdnAndPath, hykwWPData_page::get_permalink($pageid, FALSE));
+    $this->assertEquals('/parent/static1', hykwWPData_page::get_permalink($pageid, TRUE));
+    $this->assertEquals('/parent/static1', hykwWPData_page::get_permalink($pageid));
 
-  ##### *get_title()
-  $this->assertEquals('固定ページ１', hykwWPData_page::iget_title());
-  $this->assertEquals('このサイトについて', hykwWPData_page::get_title(FALSE, 1752));
-  $this->assertEquals('このサイトについて', hykwWPData_page::get_title('/about', FALSE));
-  $this->assertEquals('このサイトについて', hykwWPData_page::get_title('/about'));
+    ##### *get_title()
+    $this->assertEquals('固定ページ１', hykwWPData_page::iget_title());
+    $this->assertEquals('このサイトについて', hykwWPData_page::get_title(FALSE, 1752));
+    $this->assertEquals('このサイトについて', hykwWPData_page::get_title('/about', FALSE));
+    $this->assertEquals('このサイトについて', hykwWPData_page::get_title('/about'));
 
-  ##### *get_contents()
-  $expects = array("あああああ", "いいいいい", '');
-  $this->assertEquals($expects, explode("\r\n", hykwWPData_page::iget_contents()));
-  $this->assertEquals($expects, explode("\r\n", hykwWPData_page::get_contents(FALSE, $pageid)));
+    ##### *get_contents()
+    $expects = array("あああああ", "いいいいい", '');
+    $this->assertEquals($expects, explode("\r\n", hykwWPData_page::iget_contents()));
+    $this->assertEquals($expects, explode("\r\n", hykwWPData_page::get_contents(FALSE, $pageid)));
 
-  ##### *get_date()
-  $this->assertEquals('2014/12/28', hykwWPData_page::iget_date());
-  $this->assertEquals('2014/12/28', hykwWPData_page::get_date($pageid));
+    ##### *get_date()
+    $this->assertEquals('2014/12/28', hykwWPData_page::iget_date());
+    $this->assertEquals('2014/12/28', hykwWPData_page::get_date($pageid));
 
   }
 
