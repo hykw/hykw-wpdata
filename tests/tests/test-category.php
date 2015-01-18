f168f922108b931fdd6fc3756a0f332ff3c404d4
hykw <hykw1234@gmail.com>
2015-01-18 16:49:54 +0900

    test-category

--- tests/tests/test-category.php
+++ tests/tests/test-category.php
@@ -1,114 +1,82 @@
 <?php
 
-class UT_hykwWPData_category extends WP_UnitTestCase {
-  private $postids;
-  private $catids;
+class UT_hykwWPData_category extends hykwEasyUT {
 
-  public function setUp()
+  public function test_get_post_objects()
   {
-    parent::setUp();
 
-    switch_theme('wptest');
+    $postid = 4;
+    $url = sprintf('/archives/%d?code=3', $postid);
+    $this->go_to($url);
 
-    /*                 term_id
-        catid1         2
-          catid1_1     3
-          catid1_2     4
-        catid2         5
-     */
-
-    $catid1 = $this->factory->term->create( array(
-      'name' => 'cat1_parent',
-      'taxonomy' => 'category',
-      'description', 'desc - cat1',
-      ));
-    $catid1_1 = $this->factory->term->create_object( array(
-      'name' => 'cat1_child1',
-      'taxonomy' => 'category',
-      'description' => 'desc - cat1_1',
-      'parent' => $catid1,
-      ));
-    $catid1_2 = $this->factory->term->create_object( array(
-      'name' => 'cat1_child2',
-      'taxonomy' => 'category',
-      'description' => 'desc - cat1_2',
-      'parent' => $catid1,
-      ));
-
-    $catid2 = $this->factory->term->create( array(
-      'name' => 'cat2',
-      'taxonomy' => 'category',
-      'description', 'desc - cat2',
-      ));
-
-    $this->catids = array(
-      $catid1,
-      $catid1_1,
-      $catid1_2,
-      $catid2,
+    $expects = array(
+      array('name' => 'カテゴリ01'),
+      array('name' => '子カテゴリー 01'),
+      array('name' => '親カテゴリー'),
     );
+    $this->assertEquals($expects, hykwWPData_category::iget_post_objects(array('name')));
+    $this->assertEquals($expects, hykwWPData_category::get_post_objects($postid, array('name')));
+    $this->assertEquals($expects, hykwWPData_category::iget_post_objects(array('name'), FALSE));
+    $this->assertEquals($expects, hykwWPData_category::get_post_objects($postid, array('name'), FALSE));
 
-#    $cat_all = get_terms( "category", "fields=all&get=all"  );
-#    print_r( $cat_all);
-
-    /*              category          post_id
-      postid1       catid1            3
-        postid1_1   catid1_1          4
-        postid1_2   catid1, catid1_2  5
-      postid2       catid2            6
-     */
-    $postid1 = $this->factory->post->create( array(
-      'post_type' => 'post',
-      'post_title' => 'post title1',
-    ));
-    $postid1_1 = $this->factory->post->create_object( array(
-      'post_type' => 'post',
-      'post_title' => 'post title 11',
-      'post_parent' => $postid1,
-    ));
-    $postid1_2 = $this->factory->post->create_object( array(
-      'post_type' => 'post',
-      'post_title' => 'post title 12',
-      'post_parent' => $postid1,
-    ));
-    $postid2 = $this->factory->post->create( array(
-      'post_type' => 'post',
-      'post_title' => 'post title2',
-    ));
-
-     wp_set_post_terms($postid1, $this->catids[0]);
-     wp_set_post_terms($postid1_1, $this->catids[1]);
-     wp_set_post_terms($postid1_2, array($this->catids[0], $this->catids[2]));
-     wp_set_post_terms($postid2, $this->catids[3]);
-
-    $this->postids = array(
-      $postid1,
-      $postid1_1,
-      $postid1_2,
-      $postid2,
+    $expects = array(
+      array('name' => 'カテゴリ01'),
+      array('name' => '親カテゴリー'),
     );
-
-  }
-
-  public function tearDown()
-  {
-    parent::tearDown();
+    $this->assertEquals($expects, hykwWPData_category::iget_post_objects(array('name'), TRUE));
+    $this->assertEquals($expects, hykwWPData_category::get_post_objects($postid, array('name'), TRUE));
   }
 
   public function test_bulk()
   {
-    /*
-#    $url = sprintf('/archives/%s', $this->postids[0]);
-    $url = sprintf('/?p=%s', $this->postids[2]);
-    $this->go_to($url);
+    $url = '/archives/category/01';
+    $urlAndCode = '/archives/category/01?code=3';
+    $this->go_to($urlAndCode);
+    $fqdnAndURL = sprintf('http://%s%s', WP_TESTS_DOMAIN, $url);
+    $fqdnAndURLAndCode = sprintf('http://%s%s?code=3', WP_TESTS_DOMAIN, $url);
+    $fqdnAndURL_4 = sprintf('http://%s%s/0101', WP_TESTS_DOMAIN, $url);
 
-    $obj = hykwWPData_category::iget_post_objects();
-    p($obj);
-    $this->assertEquals(FALSE, $obj[0]['name']);
-#    $this->assertEquals(FALSE, hykwWPData_category::iget_post_objects(FALSE, FALSE));
-     */
- 
+    ##### iget_id()
+    $this->assertEquals(3, hykwWPData_category::iget_id());
+
+    ##### *get_objects()
+    $expects = array(
+      'name' => 'カテゴリ01',
+    );
+    $this->assertEquals($expects, hykwWPData_category::iget_objects(array('name')));
+    $this->assertEquals($expects, hykwWPData_category::get_objects(3, array('name')));
+    $expects = array(
+      'name' => 'カテゴリ01-1',
+      'parent' => 3,
+    );
+    $this->assertEquals($expects, hykwWPData_category::get_objects(4, array('name', 'parent')));
+
+    $this->assertEquals(array(), hykwWPData_category::get_objects(9999));
+
+    ##### *get_childObjects()
+    $expects = array(
+      array('name' => 'カテゴリ01-1'),
+      array('name' => 'カテゴリ01-2'),
+    );
+    $this->assertEquals($expects, hykwWPData_category::iget_childObjects(array('name')));
+    $this->assertEquals($expects, hykwWPData_category::get_childObjects(3, array('name')));
+    $this->assertEquals(array(), hykwWPData_category::get_childObjects(9999));
+
+    $this->assertEquals($fqdnAndURL, hykwWPData_category::iget_permalink(FALSE));
+    $this->assertEquals($fqdnAndURL, hykwWPData_category::get_permalink(3, FALSE));
+    $this->assertEquals($fqdnAndURL_4, hykwWPData_category::get_permalink(4, FALSE));
+    $this->assertEquals('', hykwWPData_category::get_permalink(9999));
+
+    $this->assertEquals('/archives/category/01', hykwWPData_category::iget_permalink());
+    $this->assertEquals('/archives/category/01', hykwWPData_category::iget_permalink(TRUE));
+    $this->assertEquals('/archives/category/01', hykwWPData_category::get_permalink(3));
+    $this->assertEquals('/archives/category/01', hykwWPData_category::get_permalink(3, TRUE));
+    $this->assertEquals('/archives/category/01/0101', hykwWPData_category::get_permalink(4));
+    $this->assertEquals('/archives/category/01/0101', hykwWPData_category::get_permalink(4, TRUE));
+
+    $this->assertEquals($fqdnAndURLAndCode, hykwWPData_url::get_thisurl(TRUE));
+    $this->assertEquals($fqdnAndURL, hykwWPData_url::get_thisurl(FALSE));
+    $this->assertEquals($fqdnAndURL, hykwWPData_url::get_thisurl());
   }
-
 
 }
