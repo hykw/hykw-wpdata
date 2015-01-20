93a19ad140e0bf0c197b219430b9eeb78188c692
hykw <hykw1234@gmail.com>
2015-01-20 13:52:00 +0900

    hykwWPData_page::*get_pagemeta()を追加

--- tests/tests/test-page.php
+++ tests/tests/test-page.php
@@ -88,5 +88,18 @@ class UT_hykwWPData_page extends hykwEasyUT {
     $this->assertEquals(FALSE, hykwWPData_page::iget_title());
   }
 
+  public function test_get_postmeta()
+  {
+    $url = '/sample-page-2';
+    $this->go_to($url);
+
+    $this->assertEquals('', hykwWPData_page::iget_pagemeta('xxxxx'));
+
+    $this->assertEquals('12345', hykwWPData_page::iget_pagemeta('固定ページのカスタムフィールド'));
+    $this->assertEquals('12345', hykwWPData_page::get_pagemeta(1751, '固定ページのカスタムフィールド'));
+
+  }
+
+
 }
 
